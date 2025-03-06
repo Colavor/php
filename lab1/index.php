80dfbb4187c64dddf4387dc61ac31a503a693d73
Egor Grigoryev <egorgrigoryeff@gmail.com>
2025-03-06 19:23:50 +0300

    lab1

--- lab1/index.php
+++ lab1/index.php
@@ -4,10 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <?php
-    echo "<p>Hello world</p>"
-    ?> 
+    <header class = "header">
+        <img src="./Mospolytech.jpg" alt="Logo Moscow Polytechnik University">
+        <h1>2.1. Домашняя работа:Hello world!</h1>
+    </header>
+
+    <main class = "main">
+        <p><?php echo "Hello world"?></p>
+    </main>
+    
+    <footer class = "footer">
+        <p>Создать веб-страницу с динамическим контентом. Загрузить код в удаленный репозиторий.</p>
+    </footer>
 </body>
 </html>
